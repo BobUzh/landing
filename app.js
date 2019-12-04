c464c7d143890ed2f81d3f2aa848358c39af6936
Vladimir <bobuzh@gmail.com>
2019-12-04 13:33:03 +0200

    connected isotope in section photo-gallery

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+
+    $('.photo-galery-wrap-img').isotope({
+        itemSelector:'.item',
+        layoutMode:'fitRows'
+    });
+    $('.photo-galery-wrap-tab ul li a').click(function(){
+        console.log($(this))
+        $('.photo-galery-wrap-tab ul li a').removeClass('active');
+        $(this).addClass('active');
+        
+        var selector = $(this).attr('data-filter');
+        $('.photo-galery-wrap-img').isotope({
+            filter: selector
+        });
+        return false
+    })
+
+
     $('.slider-inner').slick({
         arrows: false,
         dots: true,
